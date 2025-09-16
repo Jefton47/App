ed5bcc747d693c8399635797d16acef82d6ddf5b
Jefton47 <Jefton47@gmail.com>
2025-09-16 18:59:56 +0500

    2

--- lib/main.dart
+++ lib/main.dart
@@ -9,64 +9,68 @@ class MyApp extends  StatelessWidget{
     return MaterialApp(
       theme: ThemeData(),
 
-      home: Scaffold(
-         body:Container(
-           //------------------------------->
-           //Gradient
-           decoration: const BoxDecoration(
-             gradient: LinearGradient(
-                 begin:Alignment.topLeft,
-                end:Alignment.bottomRight,
-               colors:[
-                 Color(0xFF48B8E5),
-                 Color(0xFF4A99FA),
-               ]
-             )
-           ),
-           //<-------------------------------
-             //
-           child: Padding(
-             padding: const EdgeInsets.all(20),
-             child: Row(
-               mainAxisAlignment: MainAxisAlignment.spaceBetween, // ← раздвигаем элементы влево и вправо
-               crossAxisAlignment: CrossAxisAlignment.center,
-               children: [
-                 // Левая кнопка
-                 TextButton(
-                   onPressed: () => print("кнопка нажата"),
-                   style: TextButton.styleFrom(
-                     padding: EdgeInsets.zero,
-                   ),
-                   child: Row(
-                     children: [
-                       Image.asset('assets/images/map.png', width: 24),
-                       SizedBox(width: 20),
-                       Text(
-                         'Semarang',
-                         style: TextStyle(
-                           fontSize: 24,
-                           color: Colors.white,
-                         ),
-                       ),
-                       SizedBox(width: 20),
-                       Image.asset('assets/images/opt.png', width: 24),
-                     ],
-                   ),
-                 ),
+        home: Scaffold(
+          appBar: PreferredSize(
+            preferredSize: Size.fromHeight(60), // ← высота AppBar
+            child: Container(
+              decoration: const BoxDecoration(
+                gradient: LinearGradient(
+                  begin: Alignment.topLeft,
+                  end: Alignment.bottomRight,
+                  colors: [
+                    Color(0xFF48B8E5),
+                    Color(0xFF4A99FA),
+                  ],
+                ),
+              ),
+              child: SafeArea( // чтобы кнопки не уехали под статус-бар
+                child: Padding(
+                  padding: const EdgeInsets.all(10),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      // Левая кнопка
+                      TextButton(
+                        onPressed: () => print("кнопка нажата"),
+                        style: TextButton.styleFrom(
+                          padding: EdgeInsets.zero,
+                        ),
+                        child: Row(
+                          children: [
+                            Image.asset('assets/images/map.png', width: 24),
+                            SizedBox(width: 10),
+                            Text(
+                              'Semarang',
+                              style: TextStyle(
+                                fontSize: 24,
+                                color: Colors.white,
+                              ),
+                            ),
+                            SizedBox(width: 20),
+                            Image.asset('assets/images/opt.png', width: 24),
+                          ],
+                        ),
+                      ),
 
-                 // Правая кнопка
-                 IconButton(
-                   onPressed: () => print("Кнопка справа нажата"),
-                   icon: Image.asset(
-                     'assets/images/notification.png',
-                     width: 24,
-                   ),
-                 ),
-               ],
-             ),
-           ),
-         ),
-      ),
+                      // Правая кнопка
+                      IconButton(
+                        onPressed: () => print("Кнопка справа нажата"),
+                        icon: Image.asset(
+                          'assets/images/notification.png',
+                          width: 24,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ),
+          body: Container(
+            color: Colors.white, // фон контента
+          ),
+
+    ),
     );
   }
 }
